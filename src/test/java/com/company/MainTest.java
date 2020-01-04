12b623e0b13bfc705032626d2abc0f67a4a2a1b7
Daniel-K1 <daniel@mobilny-biznes.pl>
2020-01-04 23:40:31 +0100

    junit tests

--- src/test/java/com/company/MainTest.java
+++ src/test/java/com/company/MainTest.java
@@ -1,53 +1,55 @@
 package com.company;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
+import java.io.*;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
-    }
-
-
-    @org.junit.jupiter.api.AfterEach
+    @AfterEach
     void tearDown() {
+        DataContainer.getInstance().getElephantsWeights().clear();
+        DataContainer.getInstance().getActualOrder().clear();
+        DataContainer.getInstance().getTargetOrder().clear();
     }
 
     @ParameterizedTest
     @DisplayName("Testing input files:")
     @MethodSource("argumentFactory")
-    void MainTest_parametrized(String fileName, int expected, String description) {
+    void MainTest_parametrized(String inputFile, String resultFile) throws IOException {
 
-        ByteArrayInputStream in = new ByteArrayInputStream("My string".getBytes());
-        FileInputStream testFile = new FileInputStream(fileName);
-        //System.setIn(testFile);
+        //given
+        InputStream testFile = getClass().getClassLoader().getResourceAsStream(inputFile);
+        System.setIn(testFile);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(Objects.requireNonNull(getClass().getClassLoader()
+                        .getResourceAsStream(resultFile))));
+
         //when
+        Main.main(null);
 
         //then
+        String expectedValue = br.readLine();
+        String actualValue = outputStream.toString().trim();
+        assertEquals(expectedValue, actualValue);
     }
 
-    private static Stream<Arguments> argumentFactory(){
+    private static Stream<Arguments> argumentFactory() {
 
         return Stream.of(
-                Arguments.of("qqq QQQ a AAA aaaaaaacvvv", 5, "(Correctly calculated mixed set of words)"),
-                Arguments.of(null, 0, "(Correctly calculated \"0\" words when String is null)"),
-                Arguments.of("",0,"(Correctly calculated words when String is empty)"),
-                Arguments.of("aaaQQQ",1,"(Correctly calculated \"1\" when one word, few chars long)"),
-                Arguments.of("q",1,"(Correctly calculated \"1\" when one word, one char long (other than whitespace))"),
-                Arguments.of("\n",0,"(Correctly calculated \"0\" when one word, one char long (whitespace))"),
-                Arguments.of("qqq   qqq    \n\n  QQQ",3,"(Correctly calculated words when separated with more than one whitespace)"),
-                Arguments.of("                   \n\n", 0,"(Correctly calculated \"0\" when only whitespaces)"),
-                Arguments.of("q w e r t y",6,"(Correctly calculating set of single char words)")
+                Arguments.of("slo1.in", "slo1.out"),
+                Arguments.of("slo2.in", "slo2.out"),
+                Arguments.of("slo3.in", "slo3.out")
         );
     }
 }
